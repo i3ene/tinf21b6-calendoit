60b8dd672f8ed5303c62bddeecaf2bf9fb625d56
Oliver Saar <oli.saar123@gmail.com>
2022-06-09 13:10:21 +0200

    created widget view #feature

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+    this.widgets = [];
+    this.widgets.push({title: 'test', actions: null, content: null});
+    this.widgets.push({title: 'test2', actions: null, content: null});
   }
+
+  widgets!: { title: string, content: any, actions: any }[];
 
 }
