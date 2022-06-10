73835ce0800092254256887d3cfef9782ac32f93
I3ene <benedikt.muell@t-online.de>
2022-06-10 17:08:11 +0200

    Added Settings Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'settings', component: SettingsComponent },
   { path: '**', redirectTo: 'dashboard' },
 ];
 
