56fc5df7175490d1e267198f4dcce45302fa5081
Illumin80 <passrock0305@gmail.com>
2022-06-11 16:49:30 +0200

    Angular Calendar component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalendarComponent } from './components/calendar/calendar.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'calendar', component: CalendarComponent },
   { path: '**', redirectTo: 'dashboard' },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,33 @@ import {MaterialModule} from './modules/material.module';
 import {DashboardComponent} from './components/dashboard/dashboard.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {WidgetComponent} from './components/dashboard/widget/widget.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { CalendarBundleModule } from './modules/calendar.module';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    WidgetComponent
+    WidgetComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    CalendarBundleModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+    FlatpickrModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
