8bc089a2b39e59dd64ac72e5d6d89b8ff1b435ac
Oliver Saar <oli.saar123@gmail.com>
2022-06-29 10:44:10 +0200

    downloadservice #enhancement

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { XsltService } from 'src/app/services/xslt.service';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {saveAs} from 'file-saver';
 
 @Component({
   selector: 'app-settings',
@@ -10,10 +13,27 @@ export class SettingsComponent implements OnInit {
 
   obj: any;
 
-  constructor(private xsltService:XsltService) { }
+  constructor(private xsltService:XsltService,
+              private http:HttpClient) { }
 
   ngOnInit(): void {
-    this.obj = this.xsltService.transformJSON("res/list.xml", "res/template.xsl"); 
+    this.obj = this.xsltService.transformJSON("res/list.xml", "res/template.xsl");
   }
+
+
+  private downloadService(url: string): Observable<Blob> {
+    return this.http.get(url, {
+      responseType: 'blob'
+    })
+  }
+
+  //function to download data
+  download(): void {
+    this.downloadService('/downloads/test.zip')
+      .subscribe(blob => saveAs(blob, 'test.zip'))
+  }
+
+
+
 
 }
