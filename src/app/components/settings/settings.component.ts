d38e652158c9e216fa41be333d894a68ef3f7b5d
i3ene <benedikt.muell@t-online.de>
2022-06-29 11:31:29 +0200

    Fixed Dependency errors

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,39 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { XsltService } from 'src/app/services/xslt.service';
-import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
-import {saveAs} from 'file-saver';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { saveAs } from 'file-saver';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
-
   obj: any;
 
-  constructor(private xsltService:XsltService,
-              private http:HttpClient) { }
+  constructor(private xsltService: XsltService, private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.obj = this.xsltService.transformJSON("res/list.xml", "res/template.xsl");
+    this.obj = this.xsltService.transformJSON(
+      'res/list.xml',
+      'res/template.xsl'
+    );
   }
-
 
   private downloadService(url: string): Observable<Blob> {
     return this.http.get(url, {
-      responseType: 'blob'
-    })
+      responseType: 'blob',
+    });
   }
 
   //function to download data
   download(): void {
-    this.downloadService('/downloads/test.zip')
-      .subscribe(blob => saveAs(blob, 'test.zip'))
+    this.downloadService('/downloads/test.zip').subscribe((blob) =>
+      saveAs(blob, 'test.zip')
+    );
   }
-
-
-
-
 }
