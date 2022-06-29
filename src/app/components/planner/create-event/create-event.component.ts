19174be54441039927b86e54049bea52c4fe771a
ThomasBaeuml <thomas_baeuml@gmx.de>
2022-06-29 13:17:23 +0200

    Implimented planner with new model

--- src/app/components/planner/create-event/create-event.component.ts
+++ src/app/components/planner/create-event/create-event.component.ts
@@ -1,23 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Habit, HabitEvent } from 'src/app/models/habit.model';
 
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
-  styleUrls: ['./create-event.component.scss']
+  styleUrls: ['./create-event.component.scss'],
 })
 export class CreateEventComponent implements OnInit {
+  events: Habit[] = [];
 
-  events:{name:string;date:Date} [] = [];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-
-  }
-
-  addEvent(name:string,date:string): void{
-    this.events.push({name:name,date:new Date(date)});
+  addEvent(
+    deadline: string,
+    habitbeginn: string,
+    habitEnd: string,
+    title: string,
+    description: string
+  ): void {
+    this.events.push(
+      new Habit({
+        deadline: new Date(deadline),
+        habitBegin: new Date(habitbeginn),
+        habitEnd: new Date(habitEnd),
+        title: title,
+        description: description,
+      })
+    );
     console.log(this.events);
   }
-
 }
