680d5464280ca5632ae48907e2d45b345d1690ef
i3ene <benedikt.muell@t-online.de>
2022-06-30 11:37:14 +0200

    Added some additional functionalities

---
+++ src/app/services/exchange.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { saveAs } from 'file-saver';
+import { AppComponent } from '../app.component';
+import { XsltService } from './xslt.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExchangeService {
+
+  constructor(private http: HttpClient, private xsltService: XsltService) { }
+
+  /**
+   * Function to download data
+   */
+  public download(): void {
+    var text = new XMLSerializer().serializeToString(this.xsltService.saveXML(AppComponent.data));
+    var blob = new Blob([text], { type: 'text/xml' });
+    saveAs(blob, 'data.xml')
+  }
+
+
+  // TODO: Upload data
+
+}
