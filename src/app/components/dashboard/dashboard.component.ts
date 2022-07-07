75141797fc0a67ceeaf8beb1036b89601125dd9d
Johannes <johannes.schmaelzle@web.de>
2022-07-07 20:32:58 +0200

    Widgets are now displayable

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,21 +1,20 @@
 import {Component, OnInit} from '@angular/core';
 
+import { XsltService } from 'src/app/services/xslt.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() {
+ xslt:XsltService;
+  constructor(xsltService:XsltService) {
+    this.xslt=xsltService;
   }
 
   ngOnInit(): void {
-    this.widgets = [];
-    this.widgets.push({title: 'test', actions: null, content: null});
-    this.widgets.push({title: 'test2', actions: null, content: null});
+   
+   this.xslt.asyncTransform("res/test.xml","res/test.xsl").then(frag=>{document.getElementById("container")?.appendChild(frag)})
   }
-
-  widgets!: { title: string, content: any, actions: any }[];
 
 }
