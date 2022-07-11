6db3a0535da1891178bf754a08ce31134e6443e6
I3ene <benedikt.muell@t-online.de>
2022-07-11 18:09:56 +0200

    Added timestamp option

--- src/app/services/xslt.service.ts
+++ src/app/services/xslt.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Injectable, Pipe } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { Event } from '../models/event.model';
 
@@ -19,10 +20,10 @@ export class XsltService {
    * @param xslPath Path to XSL
    * @returns generated DocumentFragment
    */
-  async asyncTransform(xslPath: string, xmlPath?: string): Promise<any> {
+  async asyncTransform(xslPath: string, xmlPath?: string, timeStamps?: boolean): Promise<any> {
     this.xslStylesheet = await this.asyncGetFile(xslPath);
     this.xsltProcessor.importStylesheet(this.xslStylesheet);
-    this.xmlDoc = xmlPath ? await this.asyncGetFile(xmlPath) : this.saveXML(AppComponent.data.getSaveData());
+    this.xmlDoc = xmlPath ? await this.asyncGetFile(xmlPath) : this.saveXML(AppComponent.data.getSaveData(), timeStamps);
 
     return this.xsltProcessor.transformToFragment(this.xmlDoc, document);
   }
@@ -33,8 +34,8 @@ export class XsltService {
    * @param xslPath Path to XSL
    * @returns generated JSON
    */
-  async asyncTransformJSON(xslPath: string, xmlPath?: string): Promise<any> {
-    var fragment = await this.asyncTransform(xslPath, xmlPath);
+  async asyncTransformJSON(xslPath: string, xmlPath?: string, timeStamps?: boolean): Promise<any> {
+    var fragment = await this.asyncTransform(xslPath, xmlPath, timeStamps);
     var text = fragment.firstChild?.nodeValue;
     var obj = JSON.parse(text!);
 
@@ -55,10 +56,10 @@ export class XsltService {
   /**
    * @deprecated Use {@link asyncTransform}
    */
-  transform(xslPath: string, xmlPath?: string): any {
+  transform(xslPath: string, xmlPath?: string, timeStamps?: boolean): any {
     this.xslStylesheet = this.getFile(xslPath);
     this.xsltProcessor.importStylesheet(this.xslStylesheet);
-    this.xmlDoc = xmlPath ? this.getFile(xmlPath) : this.saveXML(AppComponent.data.getSaveData());
+    this.xmlDoc = xmlPath ? this.getFile(xmlPath) : this.saveXML(AppComponent.data.getSaveData(), timeStamps);
 
     return this.xsltProcessor.transformToFragment(this.xmlDoc, document);
   }
@@ -66,8 +67,8 @@ export class XsltService {
   /**
    * @deprecated Use {@link asyncTransformJSON}
    */
-  transformJSON(xslPath: string, xmlPath?: string): any {
-    var fragment = this.transform(xslPath, xmlPath);
+  transformJSON(xslPath: string, xmlPath?: string, timeStamps?: boolean): any {
+    var fragment = this.transform(xslPath, xmlPath, timeStamps);
     var text = fragment.firstChild?.nodeValue;
     var obj = JSON.parse(text!);
 
@@ -88,13 +89,27 @@ export class XsltService {
   /**
    * Convert an Object to XML
    * @param obj Any Object
+   * @param timeStamps If timestamps should be added
    * @returns generated XMLDocument
    */
-  saveXML(obj: any) {
+  saveXML(obj: any, timeStamps?: boolean) {
     // Root Tag
     var xmlDoc = document.implementation.createDocument(null, 'root');
-    this.createObjectNode(obj, 'root', xmlDoc.documentElement);
+    var root = this.createObjectNode(obj, 'root', xmlDoc.documentElement);
     xmlDoc.documentElement.setAttribute("type", "object");
+
+    // Timestamps
+    if (timeStamps) {
+      var datetime = document.createElementNS(null, 'datetime');
+      var day = document.createElementNS(null, 'day');
+
+      const date = new Date();
+      datetime.append(new DatePipe('de-DE').transform(date, 'yyyy-MM-dd')!);
+      day.append(date.getDay().toString());
+
+      root.appendChild(datetime);
+      root.appendChild(day)
+    }
 
     // Tag Instruction
     const beginDoc = xmlDoc.createProcessingInstruction(
