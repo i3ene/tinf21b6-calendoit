731581a8b8e86f8e0501d56b4f379dadb977601a
I3ene <benedikt.muell@t-online.de>
2022-07-11 19:33:49 +0200

    Removed old UI Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,8 +19,6 @@ import { CalendarBundleModule } from './modules/calendar.module';
 import { registerLocaleData } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
 import { HabitHelpComponent } from './dialogues/habit-help/habit-help.component';
-import { CreateEventComponent } from './dialogues/create-event/create-event.component';
-import { EditEventComponent } from './dialogues/edit-event/edit-event.component';
 import { CreateEditEventComponent } from './dialogues/create-edit-event/create-edit-event.component';
 
 /**
@@ -40,8 +38,6 @@ registerLocaleData(localeDe);
     SafeHtmlPipe,
     ListHabitComponent,
     HabitHelpComponent,
-    CreateEventComponent,
-    EditEventComponent,
     CreateEditEventComponent,
   ],
   imports: [
