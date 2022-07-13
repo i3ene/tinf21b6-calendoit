ce83a2a05bad66c262e6a7cf86d3c3d6433fe777
i3ene <benedikt.muell@t-online.de>
2022-07-13 13:50:21 +0200

    Fied dependency problems

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,22 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
 
 import { XsltService } from 'src/app/services/xslt.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
- xslt:XsltService;
-  constructor(xsltService:XsltService) {
-    this.xslt=xsltService;
+
+  constructor(private xsltService: XsltService) {
   }
 
   ngOnInit(): void {
-   
-   this.xslt.asyncTransform('res/test.xsl', undefined, true).then(frag=>{document.getElementById("container")?.appendChild(frag);
-   
-  });   
+    this.xsltService.asyncTransform('res/test.xsl', undefined, true).then((frag) => {
+      document.getElementById('container')?.appendChild(frag);
+    });
   }
 }
