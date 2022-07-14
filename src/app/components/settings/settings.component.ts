09dbf1541a68907b97b4927e80d8f6c97fcf4465
Oliver Saar <oli.saar123@gmail.com>
2022-07-14 12:37:26 +0200

    file upload #enhancement

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { AppComponent } from 'src/app/app.component';
-import { Data } from 'src/app/models/data.model';
-import { ExchangeService } from 'src/app/services/exchange.service';
+import {Component, OnInit} from '@angular/core';
+import {AppComponent} from 'src/app/app.component';
+import {ExchangeService} from 'src/app/services/exchange.service';
 
 @Component({
   selector: 'app-settings',
@@ -9,12 +8,45 @@ import { ExchangeService } from 'src/app/services/exchange.service';
   styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
-  obj: any;
 
-  constructor(public exchangeService: ExchangeService) {}
+  obj: any;
+  file: any
+
+  constructor(public exchangeService: ExchangeService) {
+  }
 
   ngOnInit(): void {
-    this.obj = AppComponent.data.getSafeData();
+    this.getData();
   }
+
+  private getData(): void {
+    this.obj = AppComponent.data.getSafeData();
+
+  }
+
+  handleFileInput(event: Event) {
+
+    var reader: FileReader = new FileReader();
+    let fileToUpload: any = (event.target as HTMLInputElement)
+    var text: string = '';
+
+
+    reader.readAsArrayBuffer(fileToUpload.files[0])
+    reader.onloadend = function (res) {
+      if (res.target?.readyState != FileReader.DONE) {
+        return;
+      }
+      var arrayBuffer = res.target.result;
+      var buffer = new Uint8Array(arrayBuffer as any);
+      for (const byte of buffer) {
+        text += String.fromCharCode(byte);
+      }
+      console.log(text);
+    }
+
+    AppComponent.loadData(text, '/res/template.xsl')
+
+  }
+
 
 }
--- src/app/services/exchange.service.ts
+++ src/app/services/exchange.service.ts
@@ -1,27 +1,45 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { saveAs } from 'file-saver';
-import { AppComponent } from '../app.component';
-import { XsltService } from './xslt.service';
+import {HttpClient, HttpEvent, HttpHeaders, HttpRequest} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {saveAs} from 'file-saver';
+import {AppComponent} from '../app.component';
+import {XsltService} from './xslt.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExchangeService {
 
-  constructor(private http: HttpClient, private xsltService: XsltService) { }
+  private httpClient: any;
+
+  constructor(private http: HttpClient, private xsltService: XsltService) {
+    this.httpClient = http;
+
+  }
 
   /**
    * Function to download data
    */
   public download(): void {
-    var text = new XMLSerializer().serializeToString(this.xsltService.saveXML(AppComponent.data.getSafeData(), true));
-    var blob = new Blob([text], { type: 'text/xml' });
+    var text = new XMLSerializer().serializeToString(this.xsltService.saveXML(AppComponent.data.getSafeData()));
+    var blob = new Blob([text], {type: 'text/xml'});
     saveAs(blob, 'data.xml')
   }
 
 
-  // TODO: Upload data
+  private headers = new HttpHeaders()
+    .set('content-type', 'application/xml')
+    .set('Access-Control-Allow-Origin', '*');
+
+  postFile(fileToUpload: File): Observable<HttpEvent<any>> {
+    const endpoint = 'your-destination-url';
+    const formData: FormData = new FormData();
+    formData.append('fileKey', new Blob([fileToUpload], {type: 'text/xml'}), fileToUpload.name);
+
+
+    const req = new HttpRequest('POST', endpoint, this.headers);
+    return this.http.request(req);
+  }
+
 
 }
