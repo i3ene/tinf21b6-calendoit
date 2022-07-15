dc6770d2ededb0405cc464981d3102964fc13a80
I3ene <benedikt.muell@t-online.de>
2022-07-15 23:29:09 +0200

    Updated Dashboard UI

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { AppComponent } from 'src/app/app.component';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 import { XsltService } from 'src/app/services/xslt.service';
 @Component({
@@ -8,13 +7,22 @@ import { XsltService } from 'src/app/services/xslt.service';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-
-  constructor(private xsltService: XsltService) {
-  }
+  constructor(private xsltService: XsltService) {}
 
   ngOnInit(): void {
-    this.xsltService.asyncTransform('res/view.xsl', undefined, true).then((frag) => {
-      document.getElementById('container')?.appendChild(frag);
-    });
+    this.xsltService
+      .asyncTransform('res/view.xsl', undefined, true)
+      .then((frag) => {
+        document.getElementById('container')?.appendChild(frag);
+      });
+  }
+
+  /**
+   * Listen to external event `toggle-expand-habits`
+   * @param event Window event
+   */
+  @HostListener('window:toggle-expand-habits', ['$event'])
+  toggleExpandHabits(event: any) {
+    console.log(event);
   }
 }
