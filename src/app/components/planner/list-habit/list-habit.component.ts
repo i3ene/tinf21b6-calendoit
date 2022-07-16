55a21ea081e8ac41b90acff843b07305e4b02bcc
I3ene <benedikt.muell@t-online.de>
2022-07-16 12:25:31 +0200

    Added first concept for habbit list

--- src/app/components/planner/list-habit/list-habit.component.ts
+++ src/app/components/planner/list-habit/list-habit.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 import { Habit } from 'src/app/models/habit.model';
+import { UtilDate } from 'src/app/models/util.model';
 
 @Component({
   selector: 'app-list-habit',
@@ -7,9 +10,51 @@ import { Habit } from 'src/app/models/habit.model';
   styleUrls: ['./list-habit.component.scss'],
 })
 export class ListHabitComponent implements OnInit {
+  @Input() habits: Habit[] = [];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  displayedColumns: string[] = ['title', 'description', 'time', 'repeat', 'actions'];
+  dataSource!: MatTableDataSource<Habit>;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.updateList();
+  }
 
-  @Input() habits: Habit[] = [];
+  updateList(): void {
+    this.dataSource = new MatTableDataSource<Habit>(this.habits);
+    this.dataSource.paginator = this.paginator;
+  }
+
+  calcTimespan(element: Habit): number {
+    let timespan = element.end.getTime() - element.start.getTime();
+    let percentage = timespan / UtilDate.TIME.ONE_DAY * 100;
+    return percentage;
+  }
+
+  calcTimespanOffset(element: Habit): number {
+    let startDate = new Date(element.start);
+    startDate.setSeconds(0);
+    startDate.setMinutes(0);
+    startDate.setHours(0);
+    let timespan = element.start.getTime() - startDate.getTime();
+    let percentage = timespan / UtilDate.TIME.ONE_DAY * 100;
+    return percentage;
+  }
+
+  calcTime(element: Habit): number {
+    let idealTime = new Date(element.idealTime);
+    idealTime.setMinutes(idealTime.getMinutes() + element.duration);
+    let timespan = idealTime.getTime() - element.idealTime.getTime();
+    let percentage = timespan / (element.end.getTime() - element.start.getTime()) * 100;
+    return percentage;
+  }
+
+  calcTimeOffset(element: Habit): number {
+    let timespan = element.idealTime.getTime() - element.start.getTime();
+    let percentage = timespan / (element.end.getTime() - element.start.getTime()) * 100;
+    return percentage;
+  }
 }
